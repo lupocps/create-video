78280de394192485979756a1d95f6ab5ff61e38e
whoamihumberto <whoamihumberto@outlook.com>
2023-05-16 10:16:23 -0600

    env file and github variables

--- main.py
+++ main.py
@@ -9,7 +9,14 @@ def main():
     my_input = os.environ["INPUT_MYINPUT"]
     my_output = f"Hello {my_input}"
 
-    print(f"::set-output name=my_output::{my_output}")
+    github_actor = os.environ["GITHUB_ACTOR"]
+    github_repository = os.environ["GITHUB_REPOSITORY"]
+    print(f"Actor: {github_actor}, Repository: {github_repository}")
+
+    my_input = os.environ["INPUT_MYINPUT"]
+
+    with open(os.environ['GITHUB_ENV'], 'a', encoding='utf-8') as file:
+        file.write(f"MY_OUTPUT={my_output}\n")
 
 
 if __name__ == "__main__":
