5a3e6f753636b853cf0c2caab6d06c3ca6885979
whoamihumberto <whoamihumberto@outlook.com>
2023-05-16 10:29:52 -0600

    toc input

--- main.py
+++ main.py
@@ -6,14 +6,17 @@ import requests
 
 def main():
     ''' Main method '''
-    my_input = os.environ["INPUT_MYINPUT"]
-    my_output = f"Hello {my_input}"
+    toc = os.environ["INPUT_TOC"]
+
+    if toc:
+        with open(toc, 'r', encoding='utf-8') as toc_file:
+            print(toc_file.read())
 
     github_actor = os.environ["GITHUB_ACTOR"]
     github_repository = os.environ["GITHUB_REPOSITORY"]
     print(f"Actor: {github_actor}, Repository: {github_repository}")
 
-    my_input = os.environ["INPUT_MYINPUT"]
+    my_output = f"toc {toc}"
 
     with open(os.environ['GITHUB_OUTPUT'], 'a', encoding='utf-8') as file:
         print(f"myOutput={my_output}\n", file=file)
