297cefef5d1ef8f800abb26a5b44d606ab3cead6
Anthony Villegas <31774388+Greibus@users.noreply.github.com>
2023-05-24 12:29:41 -0600

    test printer

--- main.py
+++ main.py
@@ -4,17 +4,10 @@ import os
 #import requests
 from src.printer import print_info
 
-
 def main():
     ''' Main method '''
 
     print_info()
-
-    toc = os.environ["INPUT_TOC"]
-    my_output = f"toc {toc}"
-
-    with open(os.environ['GITHUB_OUTPUT'], 'a', encoding='utf-8') as file:
-        print(f"myOutput={my_output}\n", file=file)
 
 
 if __name__ == "__main__":
