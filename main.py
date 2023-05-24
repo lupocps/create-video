3a8fc824c5d96104bfe0e041050a3c51b1709188
whoamihumberto <whoamihumberto@outlook.com>
2023-05-24 09:50:10 -0600

    execute code from src folder

--- main.py
+++ main.py
@@ -1,21 +1,16 @@
 ''' Create Videos '''
 
 import os
-import requests
+#import requests
+from src.printer import print_info
 
 
 def main():
     ''' Main method '''
+
+    print_info()
+
     toc = os.environ["INPUT_TOC"]
-
-    if toc:
-        with open(toc, 'r', encoding='utf-8') as toc_file:
-            print(toc_file.read())
-
-    github_actor = os.environ["GITHUB_ACTOR"]
-    github_repository = os.environ["GITHUB_REPOSITORY"]
-    print(f"Actor: {github_actor}, Repository: {github_repository}")
-
     my_output = f"toc {toc}"
 
     with open(os.environ['GITHUB_OUTPUT'], 'a', encoding='utf-8') as file:
