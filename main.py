12f66a84d2aa8e9a3fcbf9f59ca686ebd692a644
Anthony Villegas <31774388+Greibus@users.noreply.github.com>
2023-05-24 16:26:28 -0600

    dict to str

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def main():
     user_details = user_verification(is_final, github_actor, github_repository)
 
     validate_yaml_file_details(yaml_dict)
-    my_output += "\n" + user_details + "\n"
+    my_output += "\n" + str(user_details) + "\n"
     print(my_output)
     print(environ['GITHUB_OUTPUT'])
     with open(environ['GITHUB_OUTPUT'], 'a', encoding='utf-8') as file:
