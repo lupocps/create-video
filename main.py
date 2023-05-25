f6ab079c2e92af19024de634a2dd29fbe8d243d1
Anthony Villegas <31774388+Greibus@users.noreply.github.com>
2023-05-25 09:23:34 -0600

    more prints in output

--- main.py
+++ main.py
@@ -33,7 +33,9 @@ def main():
     print(environ['GITHUB_OUTPUT'])
     
     with open(environ['GITHUB_OUTPUT'], 'a', encoding='utf-8') as file:
+        print("enter")
         print(f"myOutput={my_output}\n", file=file)
+        
 
 
 
