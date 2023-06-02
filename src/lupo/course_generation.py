46dbee9ab9c8e68c96b32e05cc9a767573a0c662
Anthony Villegas <31774388+Greibus@users.noreply.github.com>
2023-06-02 16:43:23 -0600

    update audio endpoint

--- src/lupo/course_generation.py
+++ src/lupo/course_generation.py
@@ -27,38 +27,38 @@ def generate_course(chapters:list, settings:Settings, azure_folder:str, course_d
 
         for section in chapter.sections:
             video = section.generate_video()
-            video_azure_link = upload_file_to_azure_blob_storage("courses", video,
-                blob_name=f"{azure_folder}/{chapter.chapter_id}-{chapter.name}/{section.name}-{section.section_id}.mp4")
-            if settings.trailer_mode:
-                settings.mail.set_video(video_azure_link, chapter.name, f"{section.tts_components.voice_speaker}/{section.name}")
-            else:
-                settings.mail.set_video(video_azure_link, chapter.name, section.name)
+          #  video_azure_link = upload_file_to_azure_blob_storage("courses", video,
+            #    blob_name=f"{azure_folder}/{chapter.chapter_id}-{chapter.name}/{section.name}-{section.section_id}.mp4")
+           # if settings.trailer_mode:
+         #       settings.mail.set_video(video_azure_link, chapter.name, f"{section.tts_components.voice_speaker}/{section.name}")
+          #  else:
+          #      settings.mail.set_video(video_azure_link, chapter.name, section.name)
 
-            captions_azure_link = ''
-            if settings.generate_captions:
+         #   captions_azure_link = ''
+        #    if settings.generate_captions:
                 #captions_azure_link = generate_captions_by_section(
           #          video, chapter, section, settings, azure_folder)
-                pass
+          #      pass
 
-            section_data = {
-                "Name": section.name,
-                "Video": video_azure_link,
-                "Captions": [caption for caption in captions_azure_link.split("\n") if caption]
-            }
+        #    section_data = {
+         #       "Name": section.name,
+         #       "Video": video_azure_link,
+          #      "Captions": [caption for caption in captions_azure_link.split("\n") if caption]
+          #  }
 
-            chapter_data["Sections"].append(section_data)
+          #  chapter_data["Sections"].append(section_data)
 
-        log(f'Video of chapter {chapter.id} generated.', 'info')
+      #  log(f'Video of chapter {chapter.id} generated.', 'info')
 
-        if settings.generate_transcript:
+       # if settings.generate_transcript:
           #  chapter_data = generate_transcript_by_chapter(
          #       chapter_data, chapter, settings, azure_folder)
-            pass
-        if settings.generate_slides:
+       #     pass
+       # if settings.generate_slides:
            # chapter_data = generate_slides_by_chapter(
            #     chapter_data, chapter, settings, azure_folder)
-            pass
-        course_data["Chapters"].append(chapter_data)
+       #     pass
+       # course_data["Chapters"].append(chapter_data)
 
     return course_data
 
